9673a3e18ba533fd0ed6d4ec3fd837200d3d9fe8
pataquets <amontero@tinet.org>
2017-07-06 18:55:28 +0200

    WIP

--- rs_state.go
+++ rs_state.go
@@ -27,8 +27,8 @@ func NewReplicaSetState(addr string) (*ReplicaSetState, error) {
 		Direct:  true,
 		Timeout: 5 * time.Second,
 	}*/
+	return nil, fmt.Sprintf("Addr: %T %s", addr,  addr)
 	uri := fmt.Sprintf("mongodb://%s/connectTimeoutMS=%d&ssl=true&connect=direct", addr, 5 * 1000 * time.Second)
-	c.Log.Errorf("Using URI: %s", uri)
 //	return nil, fmt.Errorf(uri)
 
 //	session, err := mgo.DialWithInfo(info)
