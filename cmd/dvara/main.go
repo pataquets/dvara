7976416ef83746832d7090c3bd300a708fcc51d4
pataquets <amontero@tinet.org>
2017-07-07 12:48:30 +0200

    WIP

--- cmd/dvara/main.go
+++ cmd/dvara/main.go
@@ -31,7 +31,7 @@ func Main() error {
 	maxConnections := flag.Uint("max_connections", 100, "maximum number of connections per mongo")
 	portStart := flag.Int("port_start", 6000, "start of port range")
 	portEnd := flag.Int("port_end", 6010, "end of port range")
-	addrs := flag.String("addrs", "mongossl:27017", "comma separated list of mongo addresses")
+	addrs := flag.String("addrs", "localhost:27017", "comma separated list of mongo addresses")
 
 	flag.Parse()
 
